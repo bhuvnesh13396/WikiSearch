c4265c21819ad14b5c667235505eda949a3cde56
Bhuvnesh Maheshwari <bhuvnesh13396@gmail.com>
2019-03-26 22:37:18 +0530

    Bacis lucene setup on text files

---
+++ src/WikiSearch/src/com/wikisearch/lucene/TextFileFilter.java
@@ -0,0 +1,13 @@
+package com.wikisearch.lucene;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class TextFileFilter {
+	
+	
+	   public boolean accept(File pathname) {
+	      return pathname.getName().toLowerCase().endsWith(".txt");
+	   }
+
+}
